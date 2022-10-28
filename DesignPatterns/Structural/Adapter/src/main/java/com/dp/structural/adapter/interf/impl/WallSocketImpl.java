bc7309cd99700ea047ae2cec11cb5e5291e83c94
ASUS <rahul.dhotkar143@gmail.com>
2022-10-28 13:10:35 +0530

    ass

--- DesignPatterns/Structural/Adapter/src/main/java/com/dp/structural/adapter/interf/impl/WallSocketImpl.java
+++ DesignPatterns/Structural/Adapter/src/main/java/com/dp/structural/adapter/interf/impl/WallSocketImpl.java
@@ -3,6 +3,7 @@ package com.dp.structural.adapter.interf.impl;
 import com.dp.structural.adapter.interf.WallSocket;
 import com.dp.structural.adapter.model.Volt;
 
+
 public class WallSocketImpl implements WallSocket {
 
 	public Volt getVolts() {
